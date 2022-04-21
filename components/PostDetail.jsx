eea13cf0d6937d7be44d272123068f069b2f6330
Aaron Darwish <aaron7darwish@gmail.com>
2022-04-21 14:46:18 +0100

    Troubleshooting the slug error.

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PostDetail = ({ post }) => {
   return (
     <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
-        <h1>PostDetail</h1>
+        <h1>PostDetail Test</h1>
     </div>
   )
 }
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -9,14 +9,15 @@ const PostDetails = ({ post }) => {
         <div className='container mx-auto px-10 mb-8'>
             <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
                 <div className='col-span-1 lg:col-span-8'>
-                    <PostDetail post={post} />
-                    <Author author={post.author} />
-                    <CommentsForm slug={post.slug} />
-                    <Comments slug={post.slug} />
+                    {/* <PostDetail post={post} /> */}
+                    {/* <Author author={post.author} /> */}
+                    {/* <CommentsForm slug={post.slug} /> */}
+                    {/* <Comments slug={post.slug} /> */}
+                    <PostDetail />
                 </div>
                 <div className='col-span-1 lg:col-span-4'>
                     <div className='relative lg:sticky top-8'>
-                        <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
+                        {/* <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} /> */}
                         <Categories />
                     </div>
                 </div>
